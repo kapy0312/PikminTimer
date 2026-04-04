234d51ce633caf3527c146d2b319e4c7facf9bb9
kapy0312 <kapy0312@gmail.com>
2026-04-04 17:38:50 +0800

    20260404_v1.0.0

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 必須與 GitHub 儲存庫名稱完全一致，首尾皆強制需要斜線 (Slash)
+  base: '/PikminTimer/',
 })
